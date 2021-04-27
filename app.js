f43a9f99b453adac9b437178894c5402017e9915
Kartik <kartiktomar97@gmail.com>
2021-04-27 20:00:12 +0530

    making it better

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ fs.readdirSync('./routes').map((r) => app.use('/', require(`./routes/${r}`)))
 
 if (process.env.NODE_ENV === 'production')
 {
-  app.use(express.static('./client/build'));
+  app.use(express.static('/client/build'));
   //  app.get('*', (req, res) => {
   //       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
   //   });
