c72bb39c938864c9cd041c8b2c3087385e143cdd
Kartik <kartiktomar97@gmail.com>
2021-04-28 12:03:14 +0530

    making better

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// var createError = require('http-errors');
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -19,7 +19,8 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', express.static(path.join(__dirname, '/client/build')));
+// if (process.env.NODE_ENV === 'production')
+{app.use('/', express.static(path.join(__dirname, '/client/build')));}
 //DATABASE CONNECTION
 
 mongoose.connect(
@@ -31,26 +32,26 @@ fs.readdirSync('./routes').map((r) => app.use('/', require(`./routes/${r}`)))
 // if (process.env.NODE_ENV === 'production')
 // {
 //   app.use(express.static('/client/build'));
-//   //  app.get('*', (req, res) => {
-//   //       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
-//   //   });
+   app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    });
 // }
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 app.listen(PORT, function(err){
     if (err) console.log("Error in server setup")
     console.log("Server listening on Port", PORT);
